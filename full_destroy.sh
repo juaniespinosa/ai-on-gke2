1748aa0ace9c1a7f6fe936c0c4b6ac9d712b612f
Juani Espinosa <juaniespinosa@gmail.com>
2023-10-18 00:52:54 +0000

    Destroy script creation

--- full_destroy.sh
+++ full_destroy.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+terraform -chdir="./workloads/jupyterhub/" init 
+terraform -chdir="./workloads/" init 
+terraform -chdir="./platform/" init
 
-export CLUSTERNAME=$(terraform -chdir="./platform/" output -raw cluster-name)
-export REGION=$(terraform -chdir="./platform/" output -raw region)
-export PROJECT=$(terraform -chdir="./platform/" output -raw project)
+export CLUSTERNAME=$(cat ./platform/platform.auto.tfvars | grep -w cluster_name | awk {'print $NF'}| awk -F'["]' '{ print $2 }')
+export REGION=$(cat ./platform/platform.auto.tfvars | grep -w cluster_region | awk {'print $NF'}| awk -F'["]' '{ print $2 }')
+export PROJECT=$(cat ./platform/platform.auto.tfvars | grep -w project_id | awk {'print $NF'} | awk -F'["]' '{ print $2 }')
 
 export KUBE_CONFIG_PATH=~/.kube/config 
 gcloud container clusters get-credentials $CLUSTERNAME --region $REGION --project $PROJECT 
 
 echo -e "################"
-echo -e "JUPYTERHUB"
+echo -e "JUPYTERHUB DELETE"
 echo -e "################"
 terraform -chdir="./workloads/jupyterhub/" destroy -auto-approve
 
 
 echo -e "################"
-echo -e "USER"
+echo -e "USER DELETE"
 echo -e "################"
 terraform -chdir="./workloads/" destroy -auto-approve
 
 
 echo -e "################"
-echo -e "PLATFORM	"
+echo -e "PLATFORM DELETE	"
 echo -e "################"
 terraform -chdir="./platform/" destroy -auto-approve
